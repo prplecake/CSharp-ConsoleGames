e0505912c8ec310951d2bb4c1fda525546653b1f
Matthew Jorgensen <matthew@jrgnsn.net>
2022-05-24 20:34:00 -0500

    Document RockPaperScissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -4,9 +4,16 @@ using System.Linq;
 
 namespace FMinus.ConsoleGames.RockPaperScissors
 {
+    /// <summary>
+    /// A class implementing the game Rock, Paper, Scissors.
+    /// </summary>
     public class RPS
     {
         private static readonly Random getrandom = new();
+        /// <summary>
+        /// Implements a psuedo-random number generator.
+        /// </summary>
+        /// <returns></returns>
         private static int Randoms()
         {
             lock (getrandom)
@@ -14,6 +21,11 @@ namespace FMinus.ConsoleGames.RockPaperScissors
                 return getrandom.Next(0, 3);
             }
         }
+        /// <summary>
+        /// Gets the computer's "random" choice.
+        /// </summary>
+        /// <param name="seed">A random integer from 0-2</param>
+        /// <returns></returns>
         private static string OpponentChoice(int seed)
             => new Dictionary<int, string>
             {
@@ -39,8 +51,17 @@ namespace FMinus.ConsoleGames.RockPaperScissors
             }
             return userChoice!;
         }
+        /// <summary>
+        /// Returns the score of the RPS match.
+        /// </summary>
+        /// <param name="opponentChoice">Opponent's choice</param>
+        /// <param name="userChoice">User's choice</param>
+        /// <returns></returns>
         private static string ScoreGame(string opponentChoice, string userChoice)
             => _scoreMatrix[userChoice][opponentChoice];
+        /// <summary>
+        /// Main game loop
+        /// </summary>
         public static void PlayGame()
         {
             int userScore = 0;
